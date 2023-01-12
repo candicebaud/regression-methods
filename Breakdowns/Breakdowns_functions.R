7a849f195655076b3df03fb59d6c3d884d64e602
CelGx <celine.guex@epfl.ch>
2023-01-12 11:49:05 +0100

    changes

--- Breakdowns/Breakdowns_functions.R
+++ Breakdowns/Breakdowns_functions.R
@@ -1,4 +1,9 @@
 # In this file, there are all the functions used that are linked to Breakdowns
+# In this case, y = Breakdowns is the response variable of interest.
+# A first observation we can make is that E[y]!= Var[y]. This is quite annoying
+# since we have a count data and we would wish to apply a Poisson regression.
+# But if X~Poisson, then E[X]=Var[X]. A simple transformation allows us to solve
+# this problem.
 
 # Functions ---------------------------------------------------------------
 # Modify scales of the data frame
@@ -8,6 +13,7 @@ mBreakdowns <- function(){
   df$Year  <- as.factor(df$Year)
   df$Limit <- as.factor(df$Limit)
   # Change scale
+  df$Breakdowns <- 0.5*sqrt(df$Breakdowns)
   df$Traffic <- log10(df$Traffic)
   df$HGV     <- df$HGV*100
   df$Slope   <- df$Slope*1000
@@ -22,7 +28,25 @@ mBreakdowns2 <- function(){
   df$Year  <- as.factor(df$Year)
   df$Limit <- as.factor(df$Limit)
   # Change scale
-  df$Traffic <- df$Traffic/10^7
+  df$Breakdowns <- 0.5*sqrt(df$Breakdowns)
+  df$Traffic <- df$Traffic/10^7 #Only change with the previous adjustments
+  df$HGV     <- df$HGV*100
+  df$Slope   <- df$Slope*1000
+  df$Length  <- log10(df$Length)
+  return(df)
+}
+
+# Modify scales of the data frame for negative binomial
+# We proceed the following : we consider a different manner of scaling the 
+# parameters in the data base. In particular, we won't scale y=Breakdowns any
+# more, as we do not require E[y]=Var[y] in order to use a negative binomial model.
+mBreakdowns2nb <- function(){
+  df <- Breakdowns
+  # Change to factor
+  df$Year  <- as.factor(df$Year)
+  df$Limit <- as.factor(df$Limit)
+  # Change scale
+  df$Traffic <- df$Traffic/10^7 #Only change with the previous adjustments
   df$HGV     <- df$HGV*100
   df$Slope   <- df$Slope*1000
   df$Length  <- log10(df$Length)
--- Breakdowns/Breakdowns_models.R
+++ Breakdowns/Breakdowns_models.R
@@ -9,27 +9,33 @@ library(GGally)
 source(file = "Breakdowns/Breakdowns_functions.R")
 # Loading data set --------------------------------------------------------
 load("Breakdowns/Breakdowns.RData")
-# Modify the data set so that it make sens --> explanation in "Breakdowns_functions.R"
+# Modify the data set so that it make sense --> explanations in "Breakdowns_functions.R"
 df <- mBreakdowns()
 dff <- mBreakdowns2()
-# dff$Traffic <- 10^df$Traffic/10^8 regarder le changement !!
 # Linear model ------------------------------------------------------------
 # The very first try is to fit a linear model. It is expected not to work well
 # since y=Breakdowns does not follow a Gaussian law.
-linearmodel <- lm(formula = Breakdowns~., data = df)
-summary(linearmodel)
-# Most significative variables : Year, HGV, Slope, Limit, some tunnels...
-# Multiple R-squared:  0.7325,	Adjusted R-squared:  0.7028
+ linearmodel <- lm(formula = Breakdowns~., data = df)
+ summary(linearmodel)
+# Most significant variables : Year, Direction,  HGV, Slope, Limit, some tunnels...
+# Multiple R-squared:  0.7842,	Adjusted R-squared:  0.7603
 
 # Poisson regression ------------------------------------------------------
+# Based on the first choice of changes in the scales of variables
 ### Backward selection of variables ######
 Poissonfull <- glm(Breakdowns ~ Year + HGV + Slope + Limit + Traffic+ Length + Direction+
                 Urban + Type  + SlopeType + Tunnel + Company, 
                 family="poisson", data=df)
 model <- step(Poissonfull, direction = 'backward')
-# Output: Step:  AIC=8514.27
-# Breakdowns ~ Year + HGV + Slope + Length + Direction + Tunnel
-# What about problem of high deviance ???? -->next part on glm.nb
+
+# Output: Step:  AIC=Inf
+# Breakdowns ~ Year + HGV + Slope + Limit + Traffic + Length + 
+  # Direction + Urban + Type + SlopeType + Tunnel
+# Residual deviance:  298.49  on  937  degrees of freedom
+# Slope seems to be significant, Limit maybe also
+
+# What about problem of AIC = Inf  ???? -->next part on glm.nb
+# summary(model)
 
 ### Forward selection of variables ######
 
@@ -40,37 +46,73 @@ model <- step(Poissonfull, direction = 'backward')
                  Urban + Type + Length + Limit + SlopeType + Tunnel + Company,
                direction = 'forward')
  
- # Output: Step:  AIC=8514.27
- # Breakdowns ~ Tunnel + Slope + Year + HGV + Direction + Length
- # What about the large deviance ? --> See next part
+ # Output: Step:  AIC=Inf
+ # Breakdowns ~ 1
+ # Residual deviance: 1106.2  on 1041  degrees of freedom. 1106.2/1041 = 1.062632
+ # Maybe I could have a model that suffers from over dispersion !!!
  
- # An important thing to notice here is that both backward and forward selection
- # based on AIC yields to the same choice of model :)
- 
+ # What about the large AIC ? --> See next part
+ # summary(model_e)
 
-# Second Poisson ----------------------------------------------------------
+
+# Poisson regression (2) ----------------------------------------------------
+ # Based on the second choice of changes in the scales of variables
  ### Backward selection of variables ######
  Poissonfull2 <- glm(Breakdowns ~ Year + HGV + Slope + Limit + Traffic+ Length + Direction+
                        Urban + Type  + SlopeType + Tunnel + Company, 
-                     family="poisson", data=df)
- model2 <- step(Poissonfull, direction = 'backward') 
+                     family="poisson", data=dff)
+ model2 <- step(Poissonfull2, direction = 'backward') 
  
- # Output : AIC=8507.42
- # Breakdowns ~ Year + HGV + Slope + Traffic + Length + Direction + Tunnel
+ # Output : Step:  AIC=Inf
+ # Breakdowns ~ Year + HGV + Slope + Limit + Traffic + Length + 
+   # Direction + Urban + Type + SlopeType + Tunnel
+ # Residual deviance:  298.31  on  937  degrees of freedom
+ # summary(model2)
  
  ### Forward selection of variables ######
- Poissonempty <- glm(Breakdowns ~1,        family="poisson",data=dff)
+ Poissonempty2 <- glm(Breakdowns ~1,        family="poisson",data=dff)
  
- model_e2 <- step(Poissonempty, scope = ~Year + Direction +Traffic + HGV + Slope +
+ model_e2 <- step(Poissonempty2, scope = ~Year + Direction +Traffic + HGV + Slope +
                    Urban + Type + Length + Limit + SlopeType + Tunnel + Company,
                  direction = 'forward')
  
- # Output:  Step:  AIC=8507.42
- # Breakdowns ~ Tunnel + Slope + Year + HGV + Direction + Length + Traffic
+ # Output:  Step:  AIC=Inf
+ # Breakdowns ~ 1
+ # Residual deviance: 1106.2  on 1041  degrees of freedom -> 1106.2/1041 = 1.062632
+ # Maybe I could have a model that suffers from over dispersion !!!
+ # summary(model_e2)
+ 
+ # Slightly better model than the with the previous choice of re-scaling parameters
+ # --> we keep the one found using backward selection
  
  # It seems that this improves a bit the model --> we will keep this one
  
-# Improvements ------------------------------------------------------------
- # In this section, we start from the optimal model to see if it can be improve
- # it using a way to avoid overdispersion.
+# Negative binomial model -------------------------------------------------
+ # We have previously notice a problem of over dispersion which could be solve 
+ # by using a negative binomial model.
+ # We proceed the following : we consider a different manner of scaling the 
+ # parameters in the data base. In particular, we won't scale y=Breakdowns any
+ # more, as we do not require E[y]=Var[y] in order to use a negative binomial model.
+ dfnb <- mBreakdowns2nb()
+ 
+ ### Backward selection of variables ######
+ nbfull <- glm.nb(Breakdowns ~ Year + HGV + Slope + Limit + Traffic+ Length + Direction+
+                    Urban + Type  + SlopeType + Tunnel + Company, data = dfnb)
+ nbmodel <- step(nbfull, direction = 'backward') 
+ # summary(nbmodel)
+ # Output: Step:  AIC=6475.58
+ # Breakdowns ~ Year + HGV + Slope + Direction + Tunnel
+ 
+ ### Forward selection of variables ######
+ nbempty <- glm.nb(Breakdowns ~ 1, data = dfnb)
+ nbmodel_e <- step(nbempty, scope = ~Year + Direction +Traffic + HGV + Slope +
+                     Urban + Type + Length + Limit + SlopeType + Tunnel + Company,
+                   direction = 'forward')
+ 
+ # Output: Step:  AIC=6475.58
+ # Breakdowns ~ Tunnel + Slope + Direction + Year + HGV
 
+ # Both selections of variables based on AIC yields to the same model. 
+ # We can notice that the deviance here is much higher than in the previous case.
+ # The model achieved is yet much simpler -> which one to choose ?
+ # Need to make diagnostics !
